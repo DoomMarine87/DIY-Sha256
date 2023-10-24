f1e06489db4c4f1521ed117307fc50b36b8c1227
Simon Cox <sicox123@live.co.uk>
2023-10-24 14:37:51 +0100

    Add files via upload
    Added Sha256File folder to the repository.  This folder contains the fileSha256.py file which hashes files and sample files to hash using this program.

---
+++ Sha256File/fileSha256.py
@@ -0,0 +1,28 @@
+from sha256 import get_sha256_hash
+
+attempts = 0
+
+while True:
+    try:
+        file = input("Enter file name: ")
+        with open(file,"rb") as data:
+            hash = get_sha256_hash(data.read().decode("LATIN-1"))
+    except FileNotFoundError:
+        print(f'Sorry "{file}" doesn\'t exist! Please try again: \n')
+    else:
+        print(hash)
+        break
+    finally:
+        attempts += 1
+        if attempts == 3:
+            print("Wrong file name entered three times!\nPlease check the file name and try again.")
+            break
+
+
+import hashlib
+ 
+filename = input("Enter the input file name: ")
+with open(filename,"rb") as f:
+    bytes = f.read() # read entire file as bytes
+    readable_hash = hashlib.sha256(bytes).hexdigest()
+    print(readable_hash)
